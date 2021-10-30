f1bfd32daf7b6d2f144f3a7936af486b3a413026
Sara Betancur <sarabetancurocampo22@gmail.com>
2021-10-29 20:48:51 -0500

    Se crea regsitro formula uno

--- src/formula1/Formula1.java
+++ src/formula1/Formula1.java
@@ -6,8 +6,12 @@
 package formula1;
 
 import formula1.controladores.Controlador;
+import formula1.controladores.ControladorHome;
+import formula1.modelos.Escuderia;
 import formula1.modelos.ModeloBD;
+import formula1.modelos.Piloto;
 import formula1.vistas.Vista;
+import formula1.vistas.VistaHome;
 
 /**
  *
@@ -20,10 +24,12 @@ public class Formula1 {
      */
     public static void main(String[] args) {
         
-        ModeloBD modelo = new ModeloBD();
-        Vista vista = new Vista();
-        vista.setVisible(true);
-        Controlador controlador = new Controlador(modelo,vista);
+    Escuderia escuderia= new Escuderia();
+    Piloto piloto= new Piloto();
+    VistaHome vistaHome= new VistaHome();
+    vistaHome.setVisible(true);
+    
+    ControladorHome controladorHome = new ControladorHome(escuderia,piloto,vistaHome);
         
     }
     
